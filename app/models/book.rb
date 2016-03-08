09a5cf685aa93e767e1ce5c41ee41c1f705f77b1
josephferrairo <josephferrairo@gmail.com>
2016-03-07 23:03:32 -0500

    set up controller for books

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,6 +8,6 @@ class Book < ActiveRecord::Base
   validates :published_at, presence: true
   validates :publisher_id, presence: true
   validates :page_count, numericality: { only_integer: true, greater_than: 0}
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0.0}
+  validates :price, numericality: { greater_than_or_equal_to: 0.0}
 
 end
